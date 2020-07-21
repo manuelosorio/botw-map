4e2c81ebe3655d8fb7830afd011944cd5e989475
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2020-07-21 11:02:46 -0400

    First commit

---
+++ src/assets/scripts/modules/routes.js
@@ -0,0 +1,16 @@
+let root = null;
+let useHash = true; // Defaults to: false
+let hash = '#!'; // Defaults to: '#'
+let router = new Navigo(root, useHash, hash);
+function routes() {
+  router.on(function () {
+    alert('hello router!');
+  })
+}
+// export function routes() {
+//   let root = null
+//   let useHash = true // Defaults to: false
+//   let hash = '#!' // Defaults to: '#'
+//   let route: Navigo = new Navigo(root, useHash, hash);;
+//   alert('routes')
+// }
---
+++ src/assets/scripts/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', (event) => {
+  routes();
+  $('.map-container').draggable({
+  });
+})
