e88a238930e3fb12c11e3966fecccc1115cc9a9f
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2020-07-22 23:29:46 -0400

    Add icons js file

---
+++ src/assets/scripts/modules/icons.js
@@ -0,0 +1,109 @@
+const icons = [
+  {
+    name: 'Remote Bomb (Sphere)',
+    description: 'The Remote Bomb is a Rune ability ' +
+      'for the Sheikah Slate found in Breath ' +
+      'of the Wild. It is obtained in the Ja Baij ' +
+      'Shrine while exploring the Great Plateau. ' +
+      'It can be used to: gather resources, blow up ' +
+      'enemies, and even open secret areas. The sphere bomb ' +
+      'will roll down any slope and its trajectory is ' +
+      'influenced by wind.',
+    category: 'Rune',
+    slug: 'remote-bomb-sphere'
+  },
+  {
+    name: 'Remote Bomb (Square) ',
+    description: 'The Remote Bomb is a Rune ability ' +
+      'for the Sheikah Slate found in Breath ' +
+      'of the Wild. It is obtained in the Ja Baij ' +
+      'Shrine while exploring the Great Plateau. ' +
+      'It can be used to: gather resources, blow up ' +
+      'enemies, and even open secret areas. Unlike the sphere bomb, ' +
+      'the square variant will not roll although, like the sphere bomb, ' +
+      'it can be influenced by wind.',
+    category: 'Rune',
+    slug: 'remote-bomb-square'
+  },
+  {
+    name: 'Stasis',
+    description: 'The Stasis Rune is ability obtained ' +
+      'in the Owa Daime Shrine at the Great Plateau. ' +
+      'Stasis allows link to freeze objects in time. ' +
+      'Frozen Objects can be strike with a sword which will create kinetic energy ',
+    category: 'Rune',
+    slug: 'stasis'
+  },
+  {
+    name: 'Magnesis',
+    description: '',
+    category: 'Rune',
+    slug: 'magnesis'
+  },
+  {
+    name: 'Cryonis',
+    description: '',
+    category: 'Rune',
+    slug: 'cryonis'
+  },
+  {
+    name: 'Camera',
+    description: '',
+    category: 'rune',
+    slug: 'camera'
+  },
+  {
+    name: 'Hylian Shield',
+    description: '',
+    category: 'item',
+    slug: 'hylian-shield'
+  },
+  {
+    name: 'Master Sword',
+    description: '',
+    category: 'item',
+    slug: 'master-sword'
+  },
+  {
+    name: 'Spirit Orb',
+    description: '',
+    category: 'KeyItem',
+    slug: 'spirit-orb'
+  },
+  {
+    name: 'Korok Seeds',
+    description: '',
+    category: 'KeyItem',
+    slug: 'korok-seeds'
+  },
+  {
+    name: 'Milpha\'s Grace',
+    description: '',
+    category: 'KeyItem',
+    slug: 'milpha-grace'
+  },
+  {
+    name: 'Revali\'s Gale',
+    description: '',
+    category: 'KeyItem',
+    slug: 'revali-gale'
+  },
+  {
+    name: 'Daruk\'s Protection',
+    description: '',
+    category: 'KeyItem',
+    slug: 'daruk-protection'
+  },
+  {
+    name: 'Urbosa\'s Fury',
+    description: '',
+    category: 'KeyItem',
+    slug: 'urbosa-fury'
+  },
+  {
+    name: 'Master Torch',
+    description: '',
+    category: 'Item',
+    slug: 'master-torch'
+  }
+];
