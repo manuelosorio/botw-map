8d8495f3df69c1f3afc39fe2ca380057a78ead4c
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2020-07-22 21:08:11 -0400

    Set position of map icons

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,5 +1,36 @@
 window.addEventListener('load', (event) => {
   routes();
-  $('.map-container').draggable({
-  });
+  let $icons = $('.icon')
+  let $map = $('.map-container')
+  let startPos;
+  // $icons.draggable();
+  let x1 = '0'
+  let x2 = '-2058'
+  let y1 =  '0'
+  let y2 = '-2239'
+  $map.draggable({
+    // containment: [x1, y1, x2, y2]
+      drag: function (e, ui) {
+        if (ui.position.left < x1 && ui.position.left > x2 && ui.position.left !== x2 && (ui.position.left !== x1 || ui.position.left !== x2)) {
+          console.log('can move x')
+          return true;
+        } else {
+          console.log('can\'t move x')
+          return false
+        }
+        if (ui.position.top < y1 && ui.position.top > y2 && (ui.position.top !== y1 || ui.position.top !== y2)) {
+          console.log('can move y')
+          return true;
+        } else {
+          console.log('can\'t move y')
+          return false;
+        }
+      }
+  })
+
+
+  $map.on('mousedown', () => {
+    console.log($map.position());
+  })
 })
+
