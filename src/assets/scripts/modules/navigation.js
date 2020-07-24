e993c6f7f63825b8e710e8a9546f5145525101de
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2020-07-24 02:28:39 -0400

    Update navigation.js to use li for nav links

--- src/assets/scripts/modules/navigation.js
+++ src/assets/scripts/modules/navigation.js
@@ -1,12 +1,16 @@
 import {icons} from "./icons";
 
 export class Nav {
-  $nav = document.getElementById('nav')
+  $nav = document.getElementById('nav-list')
   createNav() {
     for (let icon of icons) {
       const slug = icon.slug;
       const name = icon.name;
-      this.$nav.innerHTML += `<a href=\"/#/${slug}\">${name}</a>`
+      this.$nav.innerHTML += `
+      <li>
+        <a href=\"/#/${slug}\">${name}</a>
+      </li>
+      `
     }
   }
 }
